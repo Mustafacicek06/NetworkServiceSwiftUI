0d6e378df1be2599b93d131557b852789a54e1f2
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-08-28 12:21:20 +0300

    Initial Commit

---
+++ NetworkServiceSwiftUI/NetworkServiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceSwiftUIApp.swift
+//  NetworkServiceSwiftUI
+//
+//  Created by Mustafa Çiçek on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
